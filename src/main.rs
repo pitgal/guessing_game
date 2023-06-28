4e28ab5c1f3d263618d3671b1993f6ff798b1445
Piotr <pitgal@gmail.com>
2023-06-28 18:56:21 +0200

    Add files and cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess:u8 = match guess.trim().parse() {
+        let guess: u8 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
         };
